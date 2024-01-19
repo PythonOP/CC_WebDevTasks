ee88780a5a988d0b8e12e1a0a73ac2bbc2d3c7ea
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2024-01-19 22:32:15 +0530

    navbar created

---
+++ youtube_clone/src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter, Router, Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar,Feed,VideoDetail,ChannelDetail,SearchDetail } from './components';
+
+const App = () => (
+    // Wrap entire application
+    <BrowserRouter>
+        <Box sx={{ background: '#000'}}>
+           <Navbar/>
+           <Routes>
+                <Route path='/' exact element={<Feed/>} />
+                <Route path='/video/:id' element={<VideoDetail/>} />
+                <Route path='/channel/:id' element={<ChannelDetail/>} />
+                <Route path='/search/:id' element={<SearchDetail/>} />
+            </Routes> 
+        </Box>
+    </BrowserRouter>
+);
+
+export default App;
---
+++ youtube_clone/src/components/SearchDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const searchDetail = () => {
+  return (
+    <div>searchDetail</div>
+  )
+}
+
+export default searchDetail
