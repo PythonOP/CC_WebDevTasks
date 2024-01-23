1136ab7b426de48d4af7a1dda80e2cfe82455528
Soumyajit Saha <67553065+PythonOP@users.noreply.github.com>
2024-01-24 00:02:51 +0530

    reactPlayer

--- youtube_clone/src/components/Navbar.jsx
+++ youtube_clone/src/components/Navbar.jsx
@@ -1,34 +1,32 @@
 import { Stack } from '@mui/material';
-import { Link  } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
-import  SearchBar  from './SearchBar';
+import SearchBar from './SearchBar';
 
 const Navbar = () => (
-  // sx is used to give styles in a react component (inline)
-  <Stack 
-  direction='row' 
-  alignItems='center' 
-  p={2} 
-  sx={{
-    position:'sticky',
-    background:'#000',
-    top:0,
-    justifyContent:'space-between'}}
-  >    
-
-    <Link to='/' 
-    style={{ 
-      display:'flex',
-      alignItems:'center',
-      }}>
-
-      <img src={logo} alt="logo" height={45}/>
-
-      </Link>
-      <SearchBar></SearchBar>
-
+  <Stack
+    direction='row'
+    alignItems='center'
+    p={2}
+    sx={{
+      position: 'sticky',
+      background: '#000',
+      top: 0,
+      justifyContent: 'space-between',
+      zIndex: 2, // Ensure the Navbar is above other elements
+    }}
+  >
+    <Link
+      to='/'
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
   </Stack>
 );
 
-export default Navbar
+export default Navbar;
--- youtube_clone/src/components/VideoCard.jsx
+++ youtube_clone/src/components/VideoCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom"; 
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -6,13 +6,15 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
-  <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 0 }}>
+  <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: 8 }}>
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
-      <CardMedia image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
+      <CardMedia 
+        image={snippet?.thumbnails?.high?.url || demoThumbnailUrl} 
+        alt={snippet?.title} 
+        sx={{ width: { xs: '100%', sm: '358px'}, height: 180, borderRadius: '8px 8px 0 0' }} 
       />
     </Link>
-    <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px', borderRadius: '0 0 8px 8px' }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
         <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
           {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
@@ -28,4 +30,4 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
   </Card>
 );
 
-export default VideoCard
+export default VideoCard;
